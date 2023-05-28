19e029f80dfac416e7ea65cb98091741170f7708
JJCondoriPinto <juanjosecondoripinto@gmail.com>
2023-05-28 13:14:26 -0500

    enlace styles

--- server.js
+++ server.js
@@ -8,4 +8,7 @@ app.use(express.json()) // Comunicacion con json
 // Direccion a directorio public
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Aquí las rutas (listar, ver, crear, eliminar)
+
+
 app.listen(3000, () => {console.log("Listen on port 3000");});
